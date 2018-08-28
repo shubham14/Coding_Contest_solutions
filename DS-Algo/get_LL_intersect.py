dbf9b50d75fe57431e1780f0f813ed0c9b2c4823
Shubham Dash <shubham.ddash@gmail.com>
2018-08-27 22:00:55 -0400

    Updated intersection

--- DS-Algo/get_LL_intersect.py
+++ DS-Algo/get_LL_intersect.py
@@ -12,50 +12,69 @@ class Node:
         self.next = None
         
 # linkedlist class
-class LL:
-    def __init__(self, head):
-        self.head = head
-        
-    def printList(self):
-        if self.head is None:
-            print (-1)
-        while self.head:
-            print (self.head.val)
-            self.head = self.head.next
-    
-    # insert function in the linked list
-    def insert(self, x):
-        temp = Node(x)
-        if self.head is None:
-            self.head = temp
-        else:
-            temp.next = self.head
-            self.head = temp
+#class LL:
+#    def __init__(self, head):
+#        self.head = head
+#        
+#    def printList(self):
+#        if self.head is None:
+#            print (-1)
+#        while self.head:
+#            print (self.head.val)
+#            self.head = self.head.next
+#    
+#    # insert function in the linked list
+#    def insert(self, x):
+#        temp = Node(x)
+#        if self.head is None:
+#            self.head = temp
+#        else:
+#            temp.next = self.head
+#            self.head = temp
     
 
 
 # class containing the logic for getting the intersection node
 class Solution():
+    def printList(self, head):
+        if head is None:
+            print (-1)
+        while head:
+            print (head.val)
+            head = head.next
+    
+    # insert function in the linked list
+    def insert(self, x):
+        temp = Node(x)
+        if head is None:
+            head = temp
+        else:
+            temp.next = head
+            head = temp
+    
     # count the number of nodes in the linked list
     # helps offset the extra nodes
     def count(self, head):
         count = 0 
         while head:
             count += 1
+            head = head.next
         return count
     # return the value of the intersection node
     def get_intersect(self, head1, head2):
         c1 = self.count(head1)
         c2 = self.count(head2)
-        if c1 > c2:
+        if c1 >= c2:
+            d = c1-c2
             temp1 = head1
             temp2 = head2
         else:
+            d = c2-c1
             temp1 = head2
             temp2 = head1
-            
-        for i in range(d):
-            temp1 = temp1.next
+        if d != 0:
+            for i in range(d):
+                temp1 = temp1.next
         
         while temp1 and temp2:
             if temp1.val == temp2.val:
@@ -63,29 +82,29 @@ class Solution():
             else:
                 temp1 = temp1.next
                 temp2 = temp2.next
-            return -1
+        return -1
     
 
 if __name__ == "__main__":
     
+    sol = Solution()
+    
     # list definitions
     head1 = Node(1)
-    ll = LL(head1)
-    ll.insert(3)
-    ll.insert(5)
-    ll.insert(7)
+    head1.next = Node(2)
+    head1.next.next = Node(6)
+    head1.next.next.next = Node(5)
     print ("First Linked List")
-    ll.printList()
+    sol.printList(head1)
     
-    head2 = Node(1)
-    ll1 = LL(head2)
-    ll1.insert(3)
-    ll1.insert(5)
-    ll1.insert(6)
+    head2 = Node(3)
+    head2.next = Node(4)
+    head2.next.next = Node(6)
+    head2.next.next.next = Node(5)
     print ("Second Linked List")
-    ll1.printList()
-    
+    sol.printList(head2)
+
     # intersection node
-    sol = Solution()
+    print ("Intersection node value is:")
     ans = sol.get_intersect(head1, head2)
     print(ans)
